118a08309257b4a8cb9c427584eba3133ed02ae9
lidia <lidia.baciu97@gmail.com>
2021-10-25 18:34:29 +0300

    updated footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -7,7 +7,7 @@ function Footer() {
            <Container>
                <Row>
                    <Col className='text-center py-3'>
-Copyright EnaImpex
+Copyright Lidiaa :) 
                    </Col>
                </Row>
            </Container>
